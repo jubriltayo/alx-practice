736d70518c0f681aa5e9775acd1342b349b9966e
Jubril Tayo <78751604+jubriltayo@users.noreply.github.com>
2022-09-20 19:38:17 +0100

    Create task 5 - reverse_string.c

---
+++ task 5 - reverse_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+	int l, i;
+	char ch;
+	
+	for(l = 0; s[l] != '\0'; l++)
+		
+	for(i = 0; i < l / 2; i++)
+	{
+		ch = s[l];
+		s[l] = s[l - 1 - i];
+		s[l - 1 - i] = ch;
+	}

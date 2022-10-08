457f23b390590cd004862e6e999ca9afedf098b0
Jubril Tayo <78751604+jubriltayo@users.noreply.github.com>
2022-10-08 19:02:04 +0100

    Update task 9-strcpy.c

--- task 9-strcpy.c
+++ task 9-strcpy.c
@@ -14,3 +14,20 @@ char *_strcpy(char *dest, char *src);
 	
 	return (dest);
 }
+
+OR
+
+#include "main.h"
+
+char *_strcpy(char *dest, char *src)
+{
+    int i = 0;
+
+    while (src[i] != '\0')
+    {
+        dest[i] = src[i];
+        i++;
+    }
+
+    return (dest);
+}

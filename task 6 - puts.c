1adab3663e0ee7ba00c4f53cad4219cdf4f4f09f
Jubril Tayo <78751604+jubriltayo@users.noreply.github.com>
2022-10-08 19:07:08 +0100

    Update task 6 - puts.c

--- task 6 - puts.c
+++ task 6 - puts.c
@@ -12,3 +12,19 @@ void puts2(char *str)
   }
   _putchar('\n');
 }
+
+OR
+
+void puts2(char *str)
+{
+
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (str[i] % 2 == 0)
+            _putchar(str[i]);
+
+        i++;
+    }
+}

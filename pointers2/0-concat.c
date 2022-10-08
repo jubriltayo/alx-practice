395e94eb83f1a6ff4965c4450ada9f9f36104d5f
Jubril Tayo <78751604+jubriltayo@users.noreply.github.com>
2022-10-08 23:28:39 +0100

    Create 0-concat.c

---
+++ pointers2/0-concat.c
@@ -0,0 +1,18 @@
+#include "main.h"
+//joins two strings together
+
+char *_strcat(char *dest, char *src)
+{
+    int i = 0, j = 0;
+
+    while (dest[i] != '\0')
+        i++;
+
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+    return (dest);
+}

276b89a117fbdee0f845ef34e39320c08a564c53
Jubril Tayo <78751604+jubriltayo@users.noreply.github.com>
2022-11-25 17:20:59 +0100

    Create print_rev.c

---
+++ pointers1/print_rev.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+{
+      int i;
+      while (str[i] != 0)
+      {
+          i++;
+      }
+      while (i != -1)
+      {
+          i--;
+          putchar(str[i]);
+      }
+}

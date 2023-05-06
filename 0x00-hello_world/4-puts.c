693aef6a5c9679d350d9ae017df87d50971e00d9
Tayo Jubril <jubriltayo@gmail.com>
2023-05-06 21:46:30 +0100

    C- Hello World revised

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char arr[] = "\"Programming is like building a multilingual puzzle";
+
+    puts(arr);
+
+    return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    char * res = "with proper grammar, but the outcome is a piece of art,";
+    printf("%s\n", res);
+
+    return 0;
+}
